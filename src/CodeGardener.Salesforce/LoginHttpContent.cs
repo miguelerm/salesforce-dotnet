9c0b1f4a24bc62e580022ab4c0e47e9857a61ff5
Miguel Roman <miguelerm@gmail.com>
2017-08-07 08:31:25 -0600

    extract login http content to a new class

---
+++ src/CodeGardener.Salesforce/LoginHttpContent.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Text;
+
+namespace CodeGardener.Salesforce
+{
+    sealed class LoginHttpContent : StringContent
+    {
+        public LoginHttpContent(string username, string password, string token) 
+            : base(GetLoginXmlContent(username, password, token), Encoding.UTF8, "application/xml")
+        {
+
+        }
+
+        private static string GetLoginXmlContent(string username, string password, string token)
+        {
+            return "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+                "<s:Body xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">" +
+                    "<login xmlns=\"urn:enterprise.soap.sforce.com\">" +
+                        "<username>" + username + "</username>" +
+                        "<password>" + password + token + "</password>" +
+                    "</login>" +
+                "</s:Body>" +
+            "</s:Envelope>";
+        }
+    }
+}
--- src/CodeGardener.Salesforce/SoapClient.cs
+++ src/CodeGardener.Salesforce/SoapClient.cs
@@ -1,5 +1,4 @@
 ﻿using System.Net.Http;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -25,9 +24,9 @@ namespace CodeGardener.Salesforce
 
         public async Task LoginAsync(string username, string password, string token)
         {
-            var content = GetLoginXmlContent(username, password + token);
+            var content = new LoginHttpContent(username, password, token);
 
-            var response = await http.PostAsync(LoginUrl, new StringContent(content, Encoding.UTF8, "application/xml"));
+            var response = await http.PostAsync(LoginUrl, content);
 
             if (response.IsSuccessStatusCode)
             {
@@ -50,16 +49,6 @@ namespace CodeGardener.Salesforce
             };
         }
 
-        private static string GetLoginXmlContent(string username, string password)
-        {
-            return "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
-                "<s:Body xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">" +
-                    "<login xmlns=\"urn:enterprise.soap.sforce.com\">" +
-                        "<username>" + username + "</username>" +
-                        "<password>" + password + "</password>" +
-                    "</login>" +
-                "</s:Body>" +
-            "</s:Envelope>";
-        }
+        
     }
 }
